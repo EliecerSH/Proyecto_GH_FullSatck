606bcd64c8229e5a83765457efb4ba3ebbf58e78
Benjamin Guerrero <benguerrerov@duocuc.cl>
2025-06-22 20:08:50 -0400

    import de css en englishtrainerapp

--- src/components/EnglishTrainerApp.jsx
+++ src/components/EnglishTrainerApp.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect, useRef } from 'react';
-import './EnglishTrainer.css';
+import "../styles/EnglishTrainerApp.css";
 
 const EnglishTrainer = () => {
   // Estados generales
